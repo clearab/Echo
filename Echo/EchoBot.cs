abddd98e00afd6262b692bf97a9f43bbacafb56a
Andrew Clear <anclear@microsoft.com>
2019-01-15 07:47:08 +0000

    Added MessagingExtension, and a sweet cat photo

--- Echo/EchoBot.cs
+++ Echo/EchoBot.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
@@ -22,7 +24,6 @@ namespace Echo
     /// For example, the <see cref="MemoryStorage"/> object and associated
     /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
     /// </summary>
-    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
     public class EchoBot : IBot
     {
         private readonly EchoAccessors _accessors;
@@ -33,7 +34,6 @@ namespace Echo
         /// </summary>
         /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
         /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
-        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
         public EchoBot(EchoAccessors accessors, ILoggerFactory loggerFactory)
         {
             if (loggerFactory == null)
@@ -56,9 +56,6 @@ namespace Echo
         /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
         /// or threads to receive notice of cancellation.</param>
         /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
-        /// <seealso cref="BotStateSet"/>
-        /// <seealso cref="ConversationState"/>
-        /// <seealso cref="IMiddleware"/>
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
             // Handle Message activity type, which is the main activity type for shown within a conversational interface
@@ -98,6 +95,45 @@ namespace Echo
                 // Echo back to the user whatever they typed.
                 string responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'. Toodles!\n";
                 await turnContext.SendActivityAsync(responseMessage);
+            }
+            else if (turnContext.Activity.Type == ActivityTypes.Invoke)
+            {
+                try
+                {
+                    MessagingExtensionResult composeExtensionResult = new MessagingExtensionResult
+                    {
+                        Type = "result",
+                        AttachmentLayout = "list",
+                        Attachments = new List<MessagingExtensionAttachment>(),
+                    };
+
+                    ThumbnailCard card = new ThumbnailCard
+                    {
+                        Title = "I'm a thumbnail",
+                        Text = "Normally I'd be way more useful.",
+                        Images = new CardImage[] { new CardImage("http://web.hku.hk/~jmwchiu/cats/cat10.jpg") },
+                    };
+                    composeExtensionResult.Attachments.Add(card.ToAttachment().ToMessagingExtensionAttachment());
+
+                    InvokeResponse ir = new InvokeResponse
+                    {
+                        Body = new MessagingExtensionResponse
+                        {
+                            ComposeExtension = composeExtensionResult
+                        },
+                        Status = 200,
+                    };
+
+                    await turnContext.SendActivityAsync(new Activity
+                    {
+                        Value = ir,
+                        Type = ActivityTypesEx.InvokeResponse,
+                    });
+                }
+                catch (Exception ex)
+                {
+                    await turnContext.SendActivityAsync($"oops: {ex.InnerException}");
+                }
             }
             else
             {
